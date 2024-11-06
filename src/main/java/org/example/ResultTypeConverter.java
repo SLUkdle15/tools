6510d0415836caa9b6f20bff554f42177bf74641
khangld5 <khangld5@fpt.com>
2024-11-06 15:12:26 +0700

    fix bug

--- src/main/java/org/example/ResultTypeConverter.java
+++ src/main/java/org/example/ResultTypeConverter.java
@@ -3,7 +3,7 @@ package org.example;
 public class ResultTypeConverter {
     public static ExcelResultType from(CSVResultType type) {
         return switch (type) {
-            case FAILED -> ExcelResultType.U;
+            case FAILED -> ExcelResultType.NG;
             case PASSED -> ExcelResultType.OK;
             case SKIPPED -> ExcelResultType.Cancelled;
         };
